c1eda6257df8c801f82f7eadcd27b2f728d466c6
leizzer <lizzydw@gmail.com>
2012-04-03 02:13:35 -0300

    moving information from .emacs to init.el. Changing Ido settings. Adding Ruby-end

--- init.el
+++ init.el
@@ -1,5 +1,19 @@
-;; Marmalade Repo for ELPA
-;; Add this to your .emacs
-;; (package-initialize)
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
+ '(ido-mode (quote both) nil (ido))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 
