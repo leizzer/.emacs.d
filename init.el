3e653310c93deaa0f0823d0663ec46c1f4e3bea5
leizzer <lizzydw@gmail.com>
2012-04-10 13:10:59 -0300

    adding electric-pair-mode for all sessions

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
+ '(electric-pair-mode t)
  '(ido-mode (quote both) nil (ido))
  '(tool-bar-mode nil)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
