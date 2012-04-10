8d95c822b54f43ac4da4bd33dad5f04c0d157d3d
leizzer <lizzydw@gmail.com>
2012-04-09 23:41:56 -0300

    adding hooks for auto-complete-mode

--- init.el
+++ init.el
@@ -14,5 +14,18 @@
  ;; If there is more than one, they won't work right.
  )
 
+
+;; Marmalade Repository
 (package-initialize)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; Require packages
+(require 'auto-complete)
+(require 'yaml-mode)
+
+;; Hooks for Auto Complete minor mode
+(add-hook 'emacs-lisp-mode-hook 'auto-complete-mode)
+(add-hook 'ruby-mode-hook 'auto-complete-mode)
+(add-hook 'css-mode-hook 'auto-complete-mode)
+(add-hook 'sass-mode-hook 'auto-complete-mode)
+(add-hook 'html-mode 'auto-complete-mode)
